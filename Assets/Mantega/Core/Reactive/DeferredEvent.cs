c52251a8a86265e8daf9d2855554fc5be2255bc5
Andre-Messias <andreluizmessias5@gmail.com>
2026-02-12 18:37:48 -0300

    Documented DeferredEvent

--- Assets/Mantega/Core/Reactive/DeferredEvent.cs
+++ Assets/Mantega/Core/Reactive/DeferredEvent.cs
@@ -2,18 +2,60 @@ namespace Mantega.Core.Reactive
 {
     using System;
 
+    using Mantega.Core.Diagnostics;
+
+    /// <summary>
+    /// Represents an event that can be fired once with a value of type <typeparamref name="T"/> and allows listeners to be registered before or after firing.
+    /// </summary>
+    /// <remarks>DeferredEvent<T> enables deferred notification of a value to registered listeners. Callbacks
+    /// registered before the event is fired are invoked when Fire is called; callbacks registered after firing are
+    /// invoked immediately. The event can only be fired once, and subsequent calls to Fire are ignored. Use Reset to
+    /// clear the state and allow reuse of the object. This class is not thread-safe; synchronize access if used in
+    /// multithreaded scenarios.</remarks>
+    /// <typeparam name="T">The type of value associated with the event when it is fired.</typeparam>
     public class DeferredEvent<T>
     {
+        #region HasFired
+        /// <summary>
+        /// Indicates whether the event has been fired.
+        /// </summary>
         private bool _hasFired;
-        private T _value;
-        private Action<T> _listeners;
 
+        /// <summary>
+        /// Gets a value indicating whether the event has already fired.
+        /// </summary>
         public bool HasFired => _hasFired;
-        public T Value => _value;
+        #endregion
 
-        public void Register(Action<T> callback)
+        #region Value
+        /// <summary>
+        /// Indicates the value with which the event was fired.
+        /// </summary>
+        private T _value;
+
+        /// <summary>
+        /// Gets the value with which the event was fired.
+        /// </summary>
+        public T Value => _value;
+        #endregion
+
+        #region Listeners
+        /// <summary>
+        /// Internal list of listeners to be invoked when the event is fired.
+        /// </summary>
+        private Action<T> _listeners;
+        #endregion
+
+        /// <summary>
+        /// Registers a callback to be invoked when the event is ready.
+        /// </summary>
+        /// <remarks>If the result is already available when this method is called, the callback is
+        /// invoked immediately. Otherwise, the callback is invoked once the result becomes available. Multiple
+        /// callbacks may be registered and will be invoked in the order they were added.</remarks>
+        /// <param name="callback">The action to execute with the <see cref="_value"/> when it becomes available. Cannot be <see langword="null"/>.</param>
+        public DeferredEvent<T> Then(Action<T> callback)
         {
-            if (callback == null) return;
+            Validations.ValidateNotNull(callback);
 
             if (_hasFired)
             {
@@ -23,17 +65,31 @@ namespace Mantega.Core.Reactive
             {
                 _listeners += callback;
             }
+
+            return this;
         }
 
-        public void Register(Action callback)
+        /// <inheritdoc cref="Then(Action{T})"/>/>
+        public DeferredEvent<T> Then(Action callback)
         {
-            if (callback == null) return;
-            Register(_ => callback());
+            Validations.ValidateNotNull(callback);
+
+            return Then(_ => callback());
         }
 
+        /// <summary>
+        /// Invokes all registered listeners with the specified value, if this method has not already been called.
+        /// </summary>
+        /// <remarks>This method has no effect if called more than once; only the first invocation will
+        /// notify listeners. After firing, all listeners are cleared and will not be invoked again.</remarks>
+        /// <param name="value">The value to pass to each registered listener.</param>
         public void Fire(T value)
         {
-            if (_hasFired) return; 
+            if (_hasFired)
+            {
+                Log.Warning("Attempted to fire DeferredEvent more than once. This call will be ignored.");
+                return;
+            }
 
             _hasFired = true;
             _value = value;
@@ -43,6 +99,12 @@ namespace Mantega.Core.Reactive
             _listeners = null;
         }
 
+        /// <summary>
+        /// Resets the internal state of the object to its initial values.
+        /// </summary>
+        /// <remarks>Call this method to clear any stored value and remove all listeners. After calling
+        /// <c>Reset</c>, the object behaves as if it was newly created. This method is not thread-safe; ensure that no
+        /// other operations are performed concurrently.</remarks>
         public void Reset()
         {
             _hasFired = false;
@@ -51,41 +113,20 @@ namespace Mantega.Core.Reactive
         }
     }
 
-
-    public class DeferredEvent
+    /// <summary>
+    /// Represents a deferred event that can be fired without providing additional event data.
+    /// </summary>
+    /// <remarks>Use this class when you need to signal an event occurrence without passing any payload to
+    /// event handlers. This is a specialization of DeferredEvent<Unit> for scenarios where only the event notification
+    /// is required.</remarks>
+    public class DeferredEvent : DeferredEvent<Unit>
     {
-        private bool _hasFired;
-        private Action _listeners;
-
-        public bool HasFired => _hasFired;
-
-        public void Register(Action callback)
-        {
-            if (callback == null) return;
-
-            if (_hasFired)
-            {
-                callback.Invoke();
-            }
-            else
-            {
-                _listeners += callback;
-            }
-        }
-
-        public void Fire()
-        {
-            if (_hasFired) return;
-
-            _hasFired = true;
-            _listeners?.Invoke();
-            _listeners = null;
-        }
-
-        public void Reset()
-        {
-            _hasFired = false;
-            _listeners = null;
-        }
+        /// <summary>
+        /// Invokes all registered listeners, if this method has not already been called.
+        /// </summary>
+        /// <remarks>This method is a convenience overload for firing the event when no additional
+        /// information needs to be provided to event handlers. It is equivalent to calling the base Fire method with a
+        /// default unit value.</remarks>
+        public void Fire() => base.Fire(Unit.Default);
     }
 }
